2d465484308a003d2adce1fe55d9b7b9e585f07c
Gloria-21 <gloriahn_82@hotmail.com>
2021-07-25 15:45:43 +0000

    Fix js file to get the map rendered

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -31,6 +31,6 @@ function initMap() {
     });
 
     var markerCluster = new MarkerClusterer(map, markers, {
-        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+        'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
     });
 }
